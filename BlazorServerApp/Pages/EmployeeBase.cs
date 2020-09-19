fc6c1c8f4fbba8c1eef809f725ed8213a74b5f36
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2020-09-19 15:27:38 +0200

    Call Rest Api

--- BlazorServerApp/Pages/EmployeeBase.cs
+++ BlazorServerApp/Pages/EmployeeBase.cs
@@ -4,70 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
+using BlazorServerApp.Service;
 
 namespace BlazorServerApp.Pages
 {
     public class EmployeeBase :  ComponentBase   
     {
+        [Inject]
+        public IEmployee IEmployee { get; set; }
         public IEnumerable<Employee> Employees { get; set; }
 
         public string NameReturned { get; set; }
 
 
-        protected override Task OnInitializedAsync()
+        protected override  async Task OnInitializedAsync()
         {
-          //  LoadEmployee();
-            return base.OnInitializedAsync();   
+            Employees = (await IEmployee.GetEmployees()).ToList();
         }
 
-        public string GetEmployee(int id)
-        {
-            string emp = (from a in Employees
-                          where a.ID == id
-                          select a.FirstName).FirstOrDefault();
-            NameReturned = emp;
-            return emp;
-        }
-
-
-
-        private void LoadEmployee()
-        {
-            Employee employee1 = new Employee()
-            {
-                ID = 1,
-                FirstName = "A",
-                LastName = "Test",
-                Gender = "aaaa@yahoo.com",
-                //Departments = new Departments { DepartmentID = 1, DepartmentName = "Software" },
-                //Gender = Gender.Male , 
-                //PhotoPath = "Img/download.png"
-            };
-
-            Employee employee2 = new Employee()
-            {
-                ID = 1,
-                FirstName = "B",
-                LastName = "Test",
-                Gender = "bbbbb@yahoo.com",
-                //Departments = new Departments { DepartmentID = 2, DepartmentName = "HardWare" },
-                //Gender = Gender.Female , 
-                //PhotoPath = "Img/images.png"
-            };
-
-            Employee employee3 = new Employee()
-            {
-                ID = 1,
-                FirstName = "C",
-                LastName = "Test",
-                Gender = "cccc@yahoo.com",
-                //Departments = new Departments { DepartmentID = 1, DepartmentName = "Network" },
-                //Gender = Gender.Male ,
-                //PhotoPath = "Img/male.png"
-            };
-
-
-            Employees = new List<Employee> { employee1, employee2 , employee3 };
-        }
     }
 }
---
+++ BlazorServerApp/Service/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Service
+{
+    public interface IEmployee
+    {
+       Task<IEnumerable<Employee>> GetEmployees();
+
+        
+
+    }
+}
